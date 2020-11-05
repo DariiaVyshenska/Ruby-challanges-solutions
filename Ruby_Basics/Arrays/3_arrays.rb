b7b44688d1d0cf9791280fdb47e16014cf1a53e8
DariiaVyshenska <vyshenskaia@gmail.com>
2020-11-05 20:39:33 +0000

    solutions for ruby basics array exercises

---
+++ Ruby_Basics/Arrays/3_arrays.rb
@@ -0,0 +1,9 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+# my_pets.delete('lizard')
+# or
+# my_pets.delete_at(1)
+# or
+my_pets.pop
+puts "I have a pet #{my_pets[0]}!"
+# p my_pets
