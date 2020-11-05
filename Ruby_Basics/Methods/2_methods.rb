342b937adf7ad6ed3c00c13573a87c7f0e393fa5
DariiaVyshenska <vyshenskaia@gmail.com>
2020-11-05 03:04:32 +0000

    solutions for ruby basics methods exercieses

---
+++ Ruby_Basics/Methods/2_methods.rb
@@ -0,0 +1,10 @@
+def print_me
+  return "I'm printing the return value!"
+end
+
+puts print_me
+
+# without explicit return statement:
+# def print_me
+#   "I'm printing the return value!"
+# end
---
+++ Ruby_Basics/Methods/5_methods.rb
@@ -0,0 +1,9 @@
+# def car(x, y)
+#   puts "#{x} #{y}"
+# end
+
+def car(x,y)
+  return "#{x} #{y}"
+end
+
+puts car('Toyota', 'Corolla')
