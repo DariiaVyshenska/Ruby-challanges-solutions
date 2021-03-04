f3dc2795189d26344388ed65a02f794141f492e6
DariiaVyshenska <vyshenskaia@gmail.com>
2021-03-04 14:30:16 -0800

    solution 11 bug fix

--- RB101_RB109/Easy_9/11_group_anagrams.rb
+++ RB101_RB109/Easy_9/11_group_anagrams.rb
@@ -1,9 +1,10 @@
 def group_anagram(arr)
-  arr.group_by { |word| word.chars.sort.join }.each { |_, anagrams| p anagrams}
+  arr.group_by { |word| word.chars.sort.join }
+     .each { |_, anagrams| p anagrams if anagrams.size > 1}
 end
 
 words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
-          'flow', 'neon']
+          'flow', 'neon', 'somewordidontknow']
 
 group_anagram(words)
